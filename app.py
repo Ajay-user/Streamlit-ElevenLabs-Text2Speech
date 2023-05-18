7623abcff639a0322ba74b6100033dad53e3221f
ajay <ajaykrish.krishnanrb@gmail.com>
2023-05-18 20:56:53 +0530

    ElevenLabs Text to Speech

--- app.py
+++ app.py
@@ -6,25 +6,27 @@ st.image(image='./LOGO-eleven-labs.png',
          caption="Model supports multiple languages, including English, German, Polish, Spanish, Italian, French, Portuguese, and Hindi.")
 
 with st.sidebar:
+    with st.expander(label="llElevenLabs", expanded=False):
+        st.caption(
+            "The basic API has a limited number of characters. To increase this limit, you can get a free API key from [llElevenLabs](https://beta.elevenlabs.io/subscription)")
+        API_KEY = st.text_input(label="API KEY")
+
     st.title("Text to Voice")
     english = st.radio(
         label="Choose your language", options=['English', 'Multilingual'], index=0, horizontal=True)
 
     value = "I am the machine." if english == 'English' else "बस बातें अपने जैसे करते है"
     text = st.text_area(label="Enter the text here",
-                        value=value, max_chars=100)
+                        value=value, max_chars=50 if not API_KEY else None)
     voice = st.selectbox(
         label="Choose the voice", options=[v.name for v in voices()]
     )
     st.divider()
-    with st.expander(label="llElevenLabs", expanded=True):
-        st.caption(
-            "The basic API has a limited number of characters. To increase this limit, you can get a free API key from [llElevenLabs](https://beta.elevenlabs.io/subscription)")
-        API_KEY = st.text_input(label="API KEY")
+
 
 try:
     audio = generate(text=text, voice=voice, model='eleven_multilingual_v1',
-                     api_key=API_KEY if API_KEY else None)
+                     api_key=API_KEY if API_KEY else st.secrets['API_KEY'])
     st.audio(data=audio)
 except UnauthenticatedRateLimitError:
     e = UnauthenticatedRateLimitError("Unauthenticated Rate Limit Error")
